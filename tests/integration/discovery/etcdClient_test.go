75f4919503c5512a9bdf1927d3a7d22a6deac64e
Mario Castro <mariocaster@gmail.com>
2015-12-13 21:06:10 +0100

    Etcd watch function ready to finish as we need to investigate the websocket functionality in go

--- tests/integration/discovery/etcdClient_test.go
+++ tests/integration/discovery/etcdClient_test.go
@@ -5,8 +5,8 @@ import "testing"
 import "github.com/sayden/docker-commander/discovery"
 
 func TestEtcdClientIntegration(t *testing.T) {
-	e := discovery.EtcdClient{}
-	r, err := e.ListHosts("http://192.168.1.35:2379")
+	e := discovery.EtcdClient{Host: "http://192.168.1.35:2379"}
+	r, err := e.ListHosts()
 
 	if err != nil {
 		t.Fatalf("Error listing hosts %q", err)
@@ -19,4 +19,9 @@ func TestEtcdClientIntegration(t *testing.T) {
 	for _, n := range r {
 		t.Logf("Node %q in list", n.IP)
 	}
+}
+
+func TestWatch(t *testing.T) {
+	e := discovery.EtcdClient{Host: "http://192.168.1.35:2379"}
+	e.WatchHosts()
 }
