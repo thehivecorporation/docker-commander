9fd3e25a2a4defea1b96d06f409b97c9e54f732e
Mario Castro <mariocaster@gmail.com>
2015-12-14 08:53:04 +0100

    Config updated with more info

--- config/config.go
+++ config/config.go
@@ -1,5 +1,16 @@
 package config
 
-const RETHINKDB_HOST string = "localhost:28015"
-
+// APP_PORT represents web server port
 const APP_PORT string = ":8000"
+
+// SWARM_MANAGER host names
+const SWARM_MANAGER string = "http://192.168.1.35:8081"
+
+// ETCD host
+const ETCD string = "http://192.168.1.35:2379"
+
+//Environments
+const DEVELOPMENT = 0
+const PRODUCTION = 1
+
+const CURRENT_ENV = DEVELOPMENT
