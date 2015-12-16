40c4ff08e0b9a4d7c7c5519e99fcff52c01120e0
Mario Castro Contreras <mcastro@atsistemas.com>
2015-12-16 11:51:36 -0500

    GetHosts refactored to GetInfo

--- swarm/swarmClient.go
+++ swarm/swarmClient.go
@@ -2,14 +2,14 @@ package swarm
 
 // Swarm is to uncouple http.Client library
 type Swarm interface {
-	GetHosts() ([]byte, error)
+	GetInfo() ([]byte, error)
 	GetContainers() ([]byte, error)
 	GetImages() ([]byte, error)
 }
 
-// GetHosts returns hosts, agents, managers... in cluster
-func GetHosts(s Swarm) ([]byte, error) {
-	return s.GetHosts()
+// GetInfo returns hosts, agents, managers... in cluster
+func GetInfo(s Swarm) ([]byte, error) {
+	return s.GetInfo()
 }
 
 // GetContainers returns the Containers of a specific host
