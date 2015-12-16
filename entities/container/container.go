8e34a7cc459daec5c635b743c677deeec3e373f1
Mario Castro Contreras <mcastro@atsistemas.com>
2015-12-16 11:02:37 -0500

    Unused files deleted

--- entities/container/container.go
+++
@@ -1,9 +0,0 @@
-// Package container is Docker description
-package container
-
-// Container is a Docker entity
-type Container struct {
-	ID    int64  `form:"id" json:"id" binding:"required"`
-	Title string `form:"title" json:"title" binding:"required"`
-	State string `form:"state" json:"state" binding:"required"`
-}
