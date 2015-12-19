fc42d113e0d9d9fa0b788539af6bb31567904bc2
Mario Castro <mariocaster@gmail.com>
2015-12-19 11:21:24 +0100

    router logging

--- routes/routes.go
+++ routes/routes.go
@@ -4,14 +4,16 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
 
 	"github.com/sayden/docker-commander/Godeps/_workspace/src/github.com/gin-gonic/gin"
 	"github.com/sayden/docker-commander/Godeps/_workspace/src/github.com/gorilla/websocket"
 	"github.com/sayden/docker-commander/discovery"
+	"github.com/sayden/docker-commander/logger"
 	"github.com/sayden/docker-commander/socket"
 	"github.com/sayden/docker-commander/swarm"
 )
+
+var log = logger.WithField("routes")
 
 // Init routes for the entire application
 func Init(ginApp *gin.Engine, s swarm.Swarm, i discovery.InfoService) {
@@ -36,9 +38,10 @@ var wsupgrader = websocket.Upgrader{
 func initWebSocket(c *gin.Context, s swarm.Swarm, i discovery.InfoService) {
 	conn, err := wsupgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
-		fmt.Printf("Failed to set websocket upgrade: %+v", err)
+		log.Errorf("Failed to set websocket upgrade: %+v", err)
 		return
 	}
+	log.Info("Websocket started successfully")
 
 	go messageHandler(conn, s, i)
 }
@@ -47,6 +50,7 @@ func messageHandler(conn *websocket.Conn, s swarm.Swarm, i discovery.InfoService
 	for {
 		msgType, msg, err := conn.ReadMessage()
 		if err != nil {
+			log.Error(err)
 			break
 		}
 
@@ -62,7 +66,7 @@ func messageHandler(conn *websocket.Conn, s swarm.Swarm, i discovery.InfoService
 			//Ask cluster state
 			info, err := socket.GetFullInfo(s, i)
 			if err != nil {
-				log.Fatal(err)
+				log.Error("Error trying to get cluster info", err)
 				sendMessage(err, conn, msgType)
 			} else {
 				sendMessage(info, conn, msgType)
@@ -74,14 +78,14 @@ func messageHandler(conn *websocket.Conn, s swarm.Swarm, i discovery.InfoService
 				s = swarm.GetClientWithIP(ip)
 				info, err := socket.GetContainers(s, ip)
 				if err != nil {
-					log.Fatal("Error trying to get containers list", err)
+					log.Error("Error trying to get containers list", err)
 					sendMessage(err, conn, msgType)
 				} else {
 					sendMessage(info, conn, msgType)
 				}
 			} else {
-				err = errors.New("Error trying to parse json")
-				log.Fatal(err)
+				err = errors.New("Error trying to parse json with containers")
+				log.Error(err)
 				sendMessage(err, conn, msgType)
 			}
 
@@ -92,14 +96,14 @@ func messageHandler(conn *websocket.Conn, s swarm.Swarm, i discovery.InfoService
 				s = swarm.GetClientWithIP(ip)
 				info, err := socket.GetImages(s, ip)
 				if err != nil {
-					log.Fatal("Error trying to get images list", err)
+					log.Error("Error trying to get images list", err)
 					sendMessage(err, conn, msgType)
 				} else {
 					sendMessage(info, conn, msgType)
 				}
 			} else {
 				err = errors.New("Error trying to parse json")
-				log.Fatal(err)
+				log.Error(err)
 				sendMessage(err, conn, msgType)
 			}
 		default:
@@ -114,6 +118,7 @@ func sendMessage(data interface{}, conn *websocket.Conn, msgType int) {
 	var res []byte
 	json, err := json.Marshal(data)
 	if err != nil {
+		log.Error("Error trying to parse interface")
 		res = []byte(err.Error())
 	} else {
 		res = json
