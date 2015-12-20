8c1284cea5341eca6712345ce8f31d95c10d04e1
Mario Castro <mariocaster@gmail.com>
2015-12-20 15:41:14 +0100

    Added redirect to github

--- public/js/App.jsx
+++ public/js/App.jsx
@@ -5,8 +5,16 @@ import AgentsList from './components/AgentsList.jsx'
 const Paper = require('material-ui/lib/paper');
 const AppBar = require('material-ui/lib/app-bar');
 const Avatar = require('material-ui/lib/avatar');
+const FontIcon = require('material-ui/lib/font-icon');
+const IconButton = require('material-ui/lib/icon-button');
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 class App extends React.Component {
+  onClickGithub(e) {
+      console.log("Asdasd",e);
+  }
+
   render() {
     let style = {margin: "10px 5px 10px 5px"}
 
@@ -23,6 +31,13 @@ class App extends React.Component {
           <AppBar
             title="Docker Commander"
             iconElementLeft={<Avatar src="img/swarm.png"></Avatar>}
+            iconElementRight={
+              <IconButton tooltip="GitHub" style={{padding:"0px"}} onFocus={function(e){
+                  window.open("http://github.com/sayden/docker-commander");
+                }}>
+                <Avatar src="img/github.png"/>
+              </IconButton>
+              }
           />
           <Cluster style={style} cluster={this.props.Cluster} />
           <AgentsList style={style} agents={this.props.Agents} />
